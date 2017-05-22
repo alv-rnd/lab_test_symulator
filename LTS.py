8055d68012e3d1d990c1a2a8b89300ddc4e2db11
leosia <sobekx@gmail.com>
2017-05-22 15:55:46 +0200

    Dodanie konstruktora do klasy Modulet. Dodanie 'pass' do każdej klasy.

--- LTS.py
+++ LTS.py
@@ -1,10 +1,15 @@
+# -*- coding: UTF-8 -*-
+
 import pandas as pd
 import numpy as np
+
 
 class Manage:
     '''
     Klasa zarządzająca wszytkim
     '''
+    pass
+
 
 class Transport:
     '''
@@ -13,28 +18,54 @@ class Transport:
     podzielona na trumny o losowej pojemnosci z określonego zakresu (symulacja produkcji)
     wysyłanych ustaloną ilość razy na dobę
     '''
+    pass
+
+
 class Check_in:
     '''
     klasa symulująca przyjęcie modułów,
     w obrębie której nastepuje losowanie ratingów, in/out, , temperatur, cond_time i innych atrybutów skojarzonych z modułem
     zmiana statusu na 'x'
     '''
+    pass
+
+
 class Conditioning:
     '''
     klasa symulująca kondycjonownie modułu w okreslonyh z góry częstościach uzupełniania komór,
     ich pojemnościach i temperaturach.
     '''
+    pass
+
+
 class Deployment:
     '''
     Klasa symulująca przeprowandzenia testu. Zmiana statusu
     W jej obebie mają znajdować sie TR oraz WICH (dodatkowy czas 30 minut dokondycjonowania doliczany w tym typie)
     '''
+    pass
+
+
 class Analysis:
     '''
     zmiana statusu na finalny, dodanie oceny testu.
     '''
+    pass
+
+
 class Modulet:
     '''
-    klasa w obrębie której będą bedą romadzić się parametry uzyskiwanie w poszczególnych etapach procesu.
-    
+    klasa w obrębie której będą bedą gromadzić się parametry uzyskiwane w poszczególnych etapach procesu.
     '''
+    ab_kind = 'dab pab sab kab ic'.split()
+
+    def __init__(self, times, status, kind, eval, project, *args, IO=False):
+        """
+        :param times: czas życia modułu, zwiększany przy zmianie statusu
+        :param status: status modułu, zmieniany z każdym etapem
+        :param kind: randomowy numer, który będzie definiował rodzaj poduszki.[ random.randint(0, len(ab_king)) ]
+        :param eval: ocena testu ['OK', 'NOK', 'INVALID']
+        :param project: liczba określająca projekt (jedna liczba == jeden projekt) generowana z podanego zakresu
+        :param args: dodatkowe argumenty których zapomniałem zamieścić
+        :param IO: parametr IN == True lub OUT == False (domyślnie False)
+        """
