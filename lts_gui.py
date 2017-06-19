f473a7b3a39fcb14f62c4554ce870257b59bb832
leosia <sobekx@gmail.com>
2017-06-19 20:21:57 +0200

    Dwie klasy Graph1 i Graph2, w jednej jest próba matplotem w drugiej próba Graphem. Oba są z kivy.garden. Póki co opisy  wykresu wyświetla druga opcja, ale bez lini wykresu :(
    Zmieniłem kodowanie w pliku kivy na windows-1250, teraz wyświetla mi polskie czcionki itp.
    Commituje też garden.graph i garden.matplotlib bo jak pobierałem to zaciągnęły mi się do folderu tego projektu, może lepiej działa? nie wiem, ale są :)

--- lts_gui.py
+++ lts_gui.py
@@ -4,11 +4,14 @@ import kivy
 
 import LTS
 
+import numpy as np
+import math
+
 from kivy.app import App
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-import kivy.garden.matplotlib
+from kivy.graphics import Canvas
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
@@ -16,16 +19,89 @@ from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty, NumericProperty
 # from kivy.utils import get_color_from_hex
 
+from kivy.garden.graph import Graph, MeshLinePlot
+
+import matplotlib
+matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
+from kivy.garden.matplotlib import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
+
+
+
+# class Graph_1_Screen(Screen):
+#     pass
+
 
 class Graph_1(Screen):
+
     def __init__(self, *args, **kwargs):
         super(Graph_1, self).__init__(**kwargs)
 
+        def press(event):
+            print('press released from test', event.x, event.y, event.button)
 
-class Graph_2(Screen):
+        def release(event):
+            print('release released from test', event.x, event.y, event.button)
+
+        def resize(event):
+            print('resize from mpl ', event)
+
+        self.N = 5
+        self.menMeans = (20, 35, 30, 35, 27)
+        self.menStd = (2, 3, 4, 1, 2)
+
+        self.ind = np.arange(self.N)  # the x locations for the groups
+        self.width = 0.35  # the width of the bars
+
+        self.fig, self.ax = plt.subplots()
+        rects1 = self.ax.bar(self.ind, self.menMeans, self.width, color='r', yerr=self.menStd)
+
+        # add some text for labels, title and axes ticks
+        self.ax.set_ylabel('Scores')
+        self.ax.set_title('Scores by group and gender')
+        self.ax.set_xticks(self.ind + self.width)
+        self.ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+
+        def autolabel(rects):
+            # attach some text labels
+            for rect in rects:
+                height = rect.get_height()
+                self.ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+                        '%d' % int(height), ha='center', va='bottom')
+
+        self.fig.canvas.mpl_connect('button_press_event', press)
+        self.fig.canvas.mpl_connect('button_release_event', release)
+        self.fig.canvas.mpl_connect('resize_event', resize)
+
+        grp = FigureCanvasKivyAgg(self.fig)
+        grp.draw()
+
+class Graph_2_Screen(Screen):
+    pass
+
+class Graph_2(Graph):
+
+
     def __init__(self, *args, **kwargs):
         super(Graph_2, self).__init__(**kwargs)
 
+        self.xlabel="X"
+        self.ylabel="Y"
+        self.x_ticks_minor=5
+        self.x_ticks_major=25
+        self.y_ticks_major=1
+        self.y_grid_label=True
+        self.x_grid_label=True
+        self.padding=5
+        self.x_grid=True
+        self.y_grid=True
+        self.xmin=-0
+        self.xmax=100
+        self.ymin=-1
+        self.ymax=1
+        self.plot = MeshLinePlot(color=[1, 0, 0, 1])
+        self.plot.points = [(x, math.sin(x / 10.)) for x in range(0, 101)]
+        self.add_plot(self.plot)
 
 class Graph_3(Screen):
     def __init__(self, *args, **kwargs):
@@ -39,15 +115,16 @@ class Graph_4(Screen):
 
 class QuestionPopup(Popup):
 
-    test_text = "Ilość modułów wyprodukowanych w APA."
-    tc_text = "Ilość komór temperaturowych gdzie będą kondycjonowane moduły."
-    tr_text = "Ilość Test Roomów w których będą odbywać się testy."
-    wich_text = "Ilość komór Walk-in w których będą odbywać się testy."
-    trunk_text = "Ilość transportów z APA na dzień."
-    frq_text = "Częstotliwość dokładania modułów do TC przez technika wsparcia (ustaw 0 jeżeli moduły mają być dokłądane od razu). "
+    question_dict = {
+    'test_text': "Ilość modułów wyprodukowanych w APA.",
+    'tc_text': "Ilość komór temperaturowych gdzie będą kondycjonowane moduły.",
+    'tr_text': "Ilość Test Roomów w których będą odbywać się testy.",
+    'wich_text': "Ilość komór Walk-in w których będą odbywać się testy.",
+    'trunk_text': "Ilość transportów z APA na dzień.",
+    'frq_text': "Częstotliwość dokładania modułów do TC przez technika wsparcia (ustaw 0 jeżeli moduły mają być dokłądane od razu). "
+    }
 
     message = ObjectProperty()
-    test_text_py = ObjectProperty()
 
     def __init__(self, *args, **kwargs):
 
@@ -57,13 +134,13 @@ class QuestionPopup(Popup):
     def popup_dissmis(self):
             return self.dismiss()
 
-    def open(self, id):
+    def open(self, text):
         # TODO: wersja not perfect ale done, jak zostanie czasu to się to ogarnie
         # funkcja uruchamiane poprzez button "?" i wyświetlająca szczegółową informację o komórce
-        print(type(id))
-        print(id)
-        # self.message.text = id
-        super(QuestionPopup, self).open()
+        self.message.text = self.question_dict[text]
+        popup = super(QuestionPopup, self)
+        popup.open()
+
         # TODO: określić wyjście z popup'a poprzez klawisz Enter
 
 
@@ -86,7 +163,7 @@ class LtsBoxLayout(BoxLayout):
         self.time_format = "min"
         self.screens = {
             "Wykres 1": "graph_1",
-            "Wykres 2": "graph_2",
+            "Wykres 2": "graph_2_screen",
             "Wykres 3": "graph_3",
             "Wykres 4": "graph_4"
         }
@@ -107,16 +184,21 @@ class LtsBoxLayout(BoxLayout):
         # print(type(self.main_param_lts[0]))
         # print(self.event_param_lts)
 
-        # sim = LTS.Manage(self.main_param_lts[0],
-        #                  self.main_param_lts[1],
-        #                  self.main_param_lts[2],
-        #                  self.main_param_lts[3],
-        #                  self.main_param_lts[4],
-        #                  self.main_param_lts[5],
-        #                  self.main_param_lts[6],
-        #                  self.event_param_lts,
-        #                  self.time_format)
-        # sim.sim_run()
+        sim = LTS.Manage(self.main_param_lts[0],
+                         self.main_param_lts[1],
+                         self.main_param_lts[2],
+                         self.main_param_lts[3],
+                         self.main_param_lts[4],
+                         self.main_param_lts[5],
+                         self.main_param_lts[6],
+                         self.event_param_lts,
+                         self.time_format)
+        sim.sim_run()
+
+        log = sim.real_time.log
+        print(log)
+
+
 
     def change_screen(self, screen_name):
         print(screen_name, self.screens[screen_name])
@@ -146,7 +228,7 @@ class LtsApp(App):
         Window.bind(on_keyboard=self.enter_dissmis)
 
     def enter_dissmis(self, window, key, *args):
-        if key == 13:
+        if key in [13, 32]:
             return QuestionPopup.popup_dissmis(self.root.questionpopup)
 
     def build(self):
